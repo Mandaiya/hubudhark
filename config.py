f2a15802ee9d8a48239508ea95037bc78aed9933
natha <108113193+Mandaiya@users.noreply.github.com>
2024-08-14 17:46:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+BOT_USERNAME = getenv("BOT_USERNAME", "Minisvd_bot")
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Mrcutex1/dark2",
