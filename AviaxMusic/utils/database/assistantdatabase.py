5741a8558142d0d16c2d609175474683f3f0e7bd
natha <108113193+Mandaiya@users.noreply.github.com>
2024-08-14 18:27:46 +0530

    Update assistantdatabase.py

--- AviaxMusic/utils/database/assistantdatabase.py
+++ AviaxMusic/utils/database/assistantdatabase.py
@@ -1,17 +1,8 @@
-#
-# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
-#
-# This file is part of < https://github.com/ArchBots/ArchMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
 import random
 
-from ArchMusic import userbot
-from ArchMusic.core.mongo import mongodb
+from AviaxMusic import userbot
+from AviaxMusic.core.mongo import mongodb
 
 db = mongodb.assistants
 
